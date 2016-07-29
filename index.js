15b60a2cda4755565198596f9888e49a54fae5d8
imomin <imomin@gmail.com>
2016-07-29 18:07:26 -0500

    removed the commented line.

--- index.js
+++ index.js
@@ -1,15 +1,9 @@
-var nlpReminder = require('./libs/nlpReminderDrop');
-var x = nlpReminder.process('computer wake me up at 8:00 AM tomorrow morning.', function (err, data) {
-	console.log(data);
-});
+var processor = require('./utils/processor');
 
+var p = new processor();
+p.startListening();
 
-//var processor = require('./utils/processor');
-
-// var p = new processor();
-//p.startListening();
-//p.startCamera();
-
+// p.startCamera();
 
 // var faceDetec = require('./libs/cvFaceRec');
 //faceDetec.train();
